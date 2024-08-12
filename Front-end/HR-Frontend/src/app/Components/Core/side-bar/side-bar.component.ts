bb95ec24f2cbe1ed2047f50eebf360605b320880
mennausama <mennausama1682@gmail.com>
2024-08-12 14:35:35 +0300

    sideBar View

--- Front-end/HR-Frontend/src/app/Components/Core/side-bar/side-bar.component.ts
+++ Front-end/HR-Frontend/src/app/Components/Core/side-bar/side-bar.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, RouterLinkActive],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive,CommonModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
 export class SideBarComponent {
+  menuItems = [
+    { label: 'Dashboard', icon: 'fa-regular fa-table-list' ,  route: '/SideBar' },
+    { label: 'Profile', icon: 'fa-regular fa-user'  ,  route: '/SideBar' },
+    { label: 'Settings', icon: 'fa-regular fa-cog'  ,  route: '/SideBar' },
+    { label: 'Sign Out', icon: 'fa-regular fa-sign-out'  ,  route: '/SideBar' },
+  ];
 
+  activeIndex: number | null = null;
+
+  setActiveIndex(index: number): void {
+    this.activeIndex = index;
+  }
 }
