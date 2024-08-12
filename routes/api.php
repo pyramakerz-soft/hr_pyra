6e40b1397c451928b31e6a56a8331c650ee08f17
MohamedQushta <m.ashraf2121@nu.edu.eg>
2024-08-12 10:50:11 +0300

    Added assign locations to users

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\ClockController;
 use App\Http\Controllers\Api\DepartmentController;
+use App\Http\Controllers\Api\HrController;
 use App\Http\Controllers\Api\PermissionController;
 use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\UserController;
@@ -50,5 +51,5 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::apiResource('user_details', UserDetailController::class)->except('update');
     Route::apiResource('user_holidays', UserHolidayController::class)->except('update');
     Route::apiResource('user_vacations', UserVacationController::class)->except('update');
-
+    Route::post('users/{user}/locations', [HrController::class, 'assignLocationToUser'])->name('users.assignLocation');
 });
