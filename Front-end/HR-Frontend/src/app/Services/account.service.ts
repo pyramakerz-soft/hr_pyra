7ebed5d1328285c938661dd81e3610bab4c58d0a
mennausama <mennausama1682@gmail.com>
2024-08-15 11:25:16 +0300

    account service

--- Front-end/HR-Frontend/src/app/Services/account.service.ts
+++ Front-end/HR-Frontend/src/app/Services/account.service.ts
@@ -5,5 +5,23 @@ import { Injectable } from '@angular/core';
 })
 export class AccountService {
 
+  isAuthenticated = !!localStorage.getItem("token"); // Check if token exists
+
+  url="http://127.0.0.1:8000/api/auth/login"
+
   constructor() { }
+
+  private CheckToken(): void {
+    const token = localStorage.getItem("token");
+    if (token) {
+      this.isAuthenticated = true;
+      // this.r = jwtDecode(token);
+
+      
+    } else {
+      this.isAuthenticated = false;
+    }
+  }
+
+
 }
