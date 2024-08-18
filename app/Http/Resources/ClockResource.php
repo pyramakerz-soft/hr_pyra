dde486b641c75a8e07828c16df801cc98ace11dc
BelalAhmed2214 <belalahmed2214@gmail.com>
2024-08-18 10:02:29 +0300

    handle clock_in and clock_out by Am and Pm

--- app/Http/Resources/ClockResource.php
+++ app/Http/Resources/ClockResource.php
@@ -18,13 +18,13 @@ class ClockResource extends JsonResource
         if ($this->clock_out == null) {
             $clock_out = null;
         } else {
-            $clock_out = Carbon::parse($this->clock_out)->format('H:i:s');
+            $clock_out = Carbon::parse($this->clock_out)->format('H:iA');
         }
         return [
             'id' => $this->id,
             'Day' => Carbon::parse($this->clock_in)->format('l'), // Extracts the day (e.g., "Sunday")
             'Date' => Carbon::parse($this->clock_in)->format('Y-m-d'), // Extracts the date (e.g., "2024-08-18")
-            'Clock In' => Carbon::parse($this->clock_in)->format('H:i:s'), // Extracts the time (e.g., "09:20:21")
+            'Clock In' => Carbon::parse($this->clock_in)->format('H:iA'), // Extracts the time (e.g., "09:20:21")
             'Clock Out' => $clock_out,
             'Total Hours' => Carbon::parse($this->duration)->format('H:i') ?? null,
             'Location In' => $this->location->address,
