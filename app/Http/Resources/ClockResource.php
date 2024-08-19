492e4e29ac1c975280e593ee1ab7eb3468370442
BelalAhmed2214 <belalahmed2214@gmail.com>
2024-08-19 15:14:09 +0300

    Handle ClockIn If User Assigned to many locations

--- app/Http/Resources/ClockResource.php
+++ app/Http/Resources/ClockResource.php
@@ -18,9 +18,11 @@ class ClockResource extends JsonResource
         if ($this->clock_out == null) {
             $clock_out = null;
             $LocationOut = null;
+            $totalHours = null;
         } else {
             $clock_out = Carbon::parse($this->clock_out)->format('H:iA');
             $LocationOut = $this->location->address;
+            $totalHours = Carbon::parse($this->duration)->format('H:i');
         }
         return [
             'id' => $this->id,
@@ -28,7 +30,8 @@ class ClockResource extends JsonResource
             'Date' => Carbon::parse($this->clock_in)->format('Y-m-d'), // Extracts the date (e.g., "2024-08-18")
             'clockIn' => Carbon::parse($this->clock_in)->format('H:iA'), // Extracts the time (e.g., "09:20:21")
             'clockOut' => $clock_out,
-            'totalHours' => Carbon::parse($this->duration)->format('H:i') ?? null,
+            'totalHours' => $totalHours,
+
             'locationIn' => $this->location->address,
             'locationOut' => $LocationOut,
             'userId' => $this->user->id,
--- app/Http/Resources/LoginResource.php
+++ app/Http/Resources/LoginResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Auth;
@@ -21,15 +22,17 @@ class LoginResource extends JsonResource
         if (!$user_clock) {
             $is_clocked_out = true;
         }
+        $clockIn = Carbon::parse($user_clock->clock_in)->format('H:i:s') ?? null;
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'national_id' => $this->national_id,
-
             'image' => $this->image ?? null,
             'job_title' => $this->user_detail->emp_type ?? null,
             'role_name' => $this->getRoleName(),
             'is_clocked_out' => $is_clocked_out,
+            'clockIn' => $clockIn,
         ];
 
     }
