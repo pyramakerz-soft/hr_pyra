9ba72f7491ae86c676e5174482665ec5960190ba
BelalAhmed2214 <belalahmed2214@gmail.com>
2024-08-25 10:54:39 +0300

    Handle UserLocationSeeder

--- database/seeders/UserLocationSeeder.php
+++ database/seeders/UserLocationSeeder.php
@@ -17,7 +17,10 @@ class UserLocationSeeder extends Seeder
         $user_hr = User::findorFail(1);
         $location = Location::findOrFail(1);
         $user_hr->user_locations()->attach($location->id);
-        $user_admin = User::findorFail(2);
-        $user_admin->user_locations()->attach($location->id);
+        $user_manager = User::findorFail(2);
+        $user_manager->user_locations()->attach($location->id);
+        $user_employee = User::findorFail(3);
+        $user_employee->user_locations()->attach($location->id);
+
     }
 }
