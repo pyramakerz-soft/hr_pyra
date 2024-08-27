d63302004c01f941b6a70d89473976e3cc5729d9
= <=>
2024-08-27 14:13:32 +0300

    Card Chart DEsign

--- Front-end/HR-Frontend/src/app/Pages/HR/hr-dashboard/hr-dashboard.component.ts
+++ Front-end/HR-Frontend/src/app/Pages/HR/hr-dashboard/hr-dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { DashboardHeroComponent } from '../../../Components/dashboard-hero/dashboard-hero.component';
 import { DonutChartComponent } from '../../../Components/Charts/donut-chart/donut-chart.component';
 import { BarChartComponent } from '../../../Components/Charts/bar-chart/bar-chart.component';
+import { CardChartComponent } from '../../../Components/Charts/card-chart/card-chart.component';
 
 @Component({
   selector: 'app-hr-dashboard',
   standalone: true,
-  imports: [DashboardHeroComponent,DonutChartComponent,BarChartComponent],
+  imports: [DashboardHeroComponent,DonutChartComponent,BarChartComponent, CardChartComponent],
   templateUrl: './hr-dashboard.component.html',
   styleUrl: './hr-dashboard.component.css'
 })
@@ -15,5 +16,10 @@ export class HrDashboardComponent {
     { label: 'Segment 1', value: 30, color: '#FF6384' },
     { label: 'Segment 2', value: 50, color: '#36A2EB' },
     { label: 'Segment 3', value: 20, color: '#FFCE56' }
+  ];
+
+  Data = [
+    { label: 'Work From Home', icon: 'fi fi-rs-chart-pie', percentage: '24.7%' },
+    { label: 'On Site', icon: 'fi fi-tr-dot-circle', percentage: '24.7%' },
   ];
 }
