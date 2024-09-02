f5caca1cf35498d3d28ee67fa8587228183036ce
BelalAhmed2214 <belalahmed2214@gmail.com>
2024-09-02 09:06:07 +0300

    Handle clockIn formatted

--- app/Exports/UserClocksExportById.php
+++ app/Exports/UserClocksExportById.php
@@ -1,15 +1,16 @@
 <?php
+
 namespace App\Exports;
 
-use App\Http\Resources\ClockResource;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Carbon;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class UserClocksExportById implements FromCollection, WithHeadings
 {
     /**
-     * @param int $userId
+     * @param Collection $clocks
      */
     public function __construct(public Collection $clocks)
     {
@@ -21,7 +22,31 @@ class UserClocksExportById implements FromCollection, WithHeadings
     public function collection()
     {
         return $this->clocks->map(function ($clock) {
-            return new ClockResource($clock);
+            $clockIn = Carbon::parse($clock->clock_in)->addHours(3);
+            $clockOut = $clock->clock_out ? Carbon::parse($clock->clock_out)->addHours(3) : null;
+            $now = Carbon::now()->addHours(3);
+
+            if ($clockOut) {
+                $realHours = $clockIn->diffInHours($clockOut);
+            } else {
+                $realHours = $clockIn->diffInHours($now);
+            }
+
+            return [
+                'ID' => $clock->id,
+                'Day' => $clockIn->format('l'),
+                'Date' => $clockIn->format('Y-m-d'),
+                'Clock_In' => $clockIn->format('h:iA'),
+                'Clock_Out' => $clockOut ? $clockOut->format('h:iA') : null,
+                'Total_Hours' => $clock->duration ? Carbon::parse($clock->duration)->format('H:i') : null,
+                'Location_In' => $clock->location_type == "site" && $clock->clock_in ? $clock->location->address : null,
+                'Location_Out' => $clock->location_type == "site" && $clock->clock_out ? $clock->location->address : null,
+                'User_ID' => $clock->user_id,
+                'Site' => $clock->location_type,
+                'Formatted_Clock_In' => $clockIn->format('Y-m-d H:i'),
+                'Formatted_Clock_Out' => $clockOut ? $clockOut->format('Y-m-d H:i') : null,
+                'Real_Hours' => $realHours + 3, // Add 3 hours to Real Hours
+            ];
         });
     }
 
@@ -45,6 +70,7 @@ class UserClocksExportById implements FromCollection, WithHeadings
             'Site',
             'Formatted_Clock_In',
             'Formatted_Clock_Out',
+            'Real_Hours', // Add Real Hours heading
         ];
     }
 }
