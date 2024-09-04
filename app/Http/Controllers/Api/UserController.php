7a232a721841a272653cd73524d497dd27ced897
BelalAhmed2214 <belalahmed2214@gmail.com>
2024-09-04 14:27:20 +0300

    Handle store and update method in UserController

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,32 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Api\LoginRequest;
 use App\Http\Requests\Api\RegisterRequest;
 use App\Http\Requests\Api\UpdateUserRequest;
 use App\Http\Resources\Api\UserDetailResource;
 use App\Http\Resources\Api\UserResource;
-use App\Http\Resources\LoginResource;
 use App\Models\Department;
 use App\Models\User;
 use App\Models\UserDetail;
+use App\Services\Api\UserDetailService;
+use App\Services\Api\UserService;
 use App\Traits\HelperTrait;
 use App\Traits\ResponseTrait;
-use Carbon\Carbon;
+use App\Traits\UserTrait;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
-use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
 {
-    use ResponseTrait, HelperTrait;
-    public function __construct()
+    use ResponseTrait, HelperTrait, UserTrait;
+    protected $userService;
+    protected $userDetailService;
+
+    public function __construct(UserService $userService, UserDetailService $userDetailService)
     {
-        $this->middleware('auth:api')->except(['store', 'login']);
+        $this->userService = $userService;
+        $this->userDetailService = $userDetailService;
+
+        $this->middleware('auth:api')->except(['store']);
     }
 
     public function index()
@@ -89,146 +92,31 @@ class UserController extends Controller
 
     public function store(RegisterRequest $request)
     {
-
-        $finalData = [];
+        $data = [];
         $authUser = Auth::user();
 
         if (!$authUser->hasRole('Hr')) {
             return $this->returnError('You are not authorized to create users', 403);
         }
-
-        $department = Department::find((int) $request->department_id);
-        if (!$department) {
-            return $this->returnError('Invalid department selected', Response::HTTP_BAD_REQUEST);
-        }
-
-        do {
-            $departmentPrefix = substr(Str::slug($department->name), 0, 4);
-            $randomDigits = mt_rand(1000, 9999);
-            $code = strtoupper($departmentPrefix) . '-' . $randomDigits;
-        } while (User::where('code', $code)->exists());
-
-        $imageUrl = $this->uploadImage($request);
-        if (!$imageUrl) {
-            return $this->returnError('Image upload failed', Response::HTTP_BAD_REQUEST);
-        }
-
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-            'phone' => $request->phone,
-            'contact_phone' => $request->contact_phone,
-            'national_id' => $request->national_id,
-            'code' => $code,
-            'gender' => $request->gender,
-            'department_id' => (int) $request->department_id,
-            'image' => $imageUrl,
-            'serial_number' => null,
-        ]);
-
-        $finalData['user'] = $user;
-
-        $salary = $request->salary;
-        $working_hours_day = $request->working_hours_day;
-        $overtime_hours = $request->overtime_hours;
-        $hourly_rate = ($salary / 22) / $working_hours_day;
-        $overtime_hourly_rate = (($salary / 30) / $working_hours_day) * $overtime_hours;
-        $start_time = $request->start_time;
-        $end_time = $request->end_time;
-        if ($end_time <= $start_time) {
-            return $this->returnError('End time must be later than start time', Response::HTTP_BAD_REQUEST);
-        }
-
-        $userDetail = UserDetail::create([
-            'salary' => $salary,
-            'working_hours_day' => $working_hours_day,
-            'hourly_rate' => $hourly_rate,
-            'overtime_hourly_rate' => $overtime_hourly_rate,
-            'overtime_hours' => $overtime_hours,
-            'start_time' => $start_time,
-            'end_time' => $end_time,
-            'emp_type' => $request->emp_type,
-            'hiring_date' => $request->hiring_date,
-            'user_id' => $user->id,
-        ]);
-
-        $finalData['user_detail'] = $userDetail;
-
-        if (!$user || !$userDetail) {
+        $user = $this->userService->createUser($request->validated());
+        if (!$user) {
             return $this->returnError('Failed to Store User');
         }
-        //assign role to user
-        $user->syncRoles($request->input('roles', []));
 
-        // Assign Location to user
-        $newLocations = $request->input('location_id', []);
-        foreach ($newLocations as $locationId) {
-            $LocationAssignedToUser = $user->user_locations()->wherePivot('location_id', $locationId)->exists();
-            if (!$LocationAssignedToUser) {
-                $user->user_locations()->attach($locationId);
-            }
+        $userDetail = $this->userDetailService->createUserDetail($user, $request->validated());
+        if (!$userDetail) {
+            return $this->returnError('Failed to Store User Detail');
         }
-        // Assign workType to User
-        $newWorkTypes = $request->input('work_type_id', []);
-        foreach ($newWorkTypes as $workTypeId) {
-            $workTypeAssignedToUser = $user->work_types()->wherePivot('work_type_id', $workTypeId)->exists();
-            if (!$workTypeAssignedToUser) {
-                $user->work_types()->attach($workTypeId);
-            }
-        }
+        $this->assignRoles($user, $request['roles'] ?? []);
+        $this->assignLocations($user, $request['location_id'] ?? []);
+        $this->assignWorkTypes($user, $request['work_type_id'] ?? []);
 
-        return $this->returnData("data", $finalData, "User Created");
-    }
+        $data = [
+            'user' => $user,
+            'user_detail' => $userDetail,
+        ];
 
-    public function login(LoginRequest $request)
-    {
-        $credentials = $request->only('email', 'password');
-
-        $errors = [];
-
-        $user = User::where('email', $credentials['email'])->first();
-
-        if (!$user) {
-            $errors['email'] = 'Wrong Email';
-        } else {
-            if (!Hash::check($credentials['password'], $user->password)) {
-                $errors['password'] = 'Wrong password';
-            }
-        }
-
-        if (!empty($errors)) {
-            return response()->json([
-                'message' => 'Validation errors occurred.',
-                'errors' => $errors,
-            ], Response::HTTP_UNAUTHORIZED);
-        }
-
-        $token = JWTAuth::attempt($credentials);
-
-        if (!$token) {
-            return $this->returnError('You Are unauthenticated', Response::HTTP_UNAUTHORIZED);
-        }
-
-        $user = auth()->user();
-        // Check if serial number is present in the request
-        $serialNumber = $request->serial_number;
-
-        if ($user && $serialNumber) {
-            if (is_null($user->serial_number)) {
-                $user->serial_number = $serialNumber;
-                $user->save();
-            } else {
-                if ($user->serial_number !== $serialNumber) {
-                    return $this->returnError('Serial number does not match', 406);
-                }
-            }
-        }
-
-        return response()->json([
-            "result" => "true",
-            'token' => $token,
-        ], Response::HTTP_OK);
+        return $this->returnData("data", $data, "User Created");
     }
 
     public function show(User $user)
@@ -244,95 +132,69 @@ class UserController extends Controller
 
     public function update(UpdateUserRequest $request, User $user)
     {
-        $finalData = [];
+        $data = [];
         $authUser = Auth::user();
 
         if (!$authUser->hasRole('Hr')) {
             return $this->returnError('You are not authorized to update users', 403);
         }
+
         $department_id = $request->has('department_id') ? $request->department_id : $user->department_id;
-        // dd($department_id);
         $department = Department::find($department_id);
         if (!$department) {
             return $this->returnError('Invalid department selected', Response::HTTP_BAD_REQUEST);
         }
 
-        do {
-            $departmentPrefix = substr(Str::slug($department->name), 0, 4);
-            $randomDigits = mt_rand(1000, 9999);
-            $code = strtoupper($departmentPrefix) . '-' . $randomDigits;
-        } while (User::where('code', $code)->exists());
-
-        $imageUrl = $this->uploadImage($request);
-
-        if (!$imageUrl) {
-            $imageUrl = $user->image;
+        $updatedUser = $this->userService->updateUser($user, $request->validated());
+        if (!$updatedUser) {
+            return $this->returnError('Failed to Update User');
         }
 
-        // Update user information
-        $user->update([
-            'name' => $request->name ?? $user->name,
-            'email' => $request->email ?? $user->email,
-            'phone' => $request->phone ?? $user->phone,
-            'contact_phone' => $request->contact_phone ?? $user->contact_phone,
-            'national_id' => $request->national_id ?? $user->national_id,
-            'code' => $code,
-            'gender' => $request->gender ?? $user->gender,
-            'department_id' => (int) $department_id,
-            'image' => $imageUrl,
-        ]);
-
-        $finalData['user'] = $user;
-
-        // Update user detail information
         $userDetail = UserDetail::where('user_id', $user->id)->first();
         if (!$userDetail) {
             return $this->returnError('No User Detail Found for this User');
         }
-        $salary = $request->salary; // Example: 24000
-        $working_hours_day = $request->working_hours_day; // Example: 8
-        if ($working_hours_day === null || $working_hours_day == 0) {
-            $hourly_rate = 0;
-        } else {
-            $hourly_rate = ($salary / 30) / $working_hours_day;
+
+        $updatedUserDetail = $this->userDetailService->updateUserDetail($userDetail, $request->validated());
+        if (!$updatedUserDetail) {
+            return $this->returnError('Failed to Update User Detail');
         }
-        $start_time = $request->start_time ? Carbon::parse($request->start_time)->format("H:i:s") : $userDetail->start_time;
-        $end_time = $request->end_time ? Carbon::parse($request->end_time)->format("H:i:s") : $userDetail->end_time;
-        if ($end_time <= $start_time) {
-            return $this->returnError('End time must be later than start time', Response::HTTP_BAD_REQUEST);
+        if ($request->has('roles')) {
+            $this->assignRoles($user, $request->input('roles'));
         }
 
-        $userDetail->update([
-            'salary' => $salary ?? $userDetail->salary,
-            'working_hours_day' => $working_hours_day ?? $userDetail->working_hours_day,
-            'hourly_rate' => $hourly_rate,
-            'overtime_hours' => $request->overtime_hours ?? $userDetail->overtime_hours,
-            'start_time' => $start_time ?? $userDetail->start_time,
-            'end_time' => $end_time ?? $userDetail->end_time,
-            'emp_type' => $request->emp_type ?? $userDetail->emp_type,
-            'hiring_date' => $request->hiring_date ?? $userDetail->hiring_date,
-            'user_id' => $user->id,
-        ]);
-
-        $finalData['user_detail'] = $userDetail;
-
-        if (!$user || !$userDetail) {
-            return $this->returnError('Failed to Update User');
+        if ($request->has('location_id')) {
+            $this->assignLocations($user, $request->input('location_id'));
         }
 
-        // Update Assigning Roles to User
-        $newRoles = $request->input('roles') ?? $user->getRoleNames()->toArray();
-        $user->syncRoles($newRoles);
+        if ($request->has('work_type_id')) {
+            $this->assignWorkTypes($user, $request->input('work_type_id'));
+        }
 
-        // Update Assigning Locations to User
-        $newLocations = $request->input('location_id') ?? $user->user_locations()->pluck('locations.id')->toArray();
-        $user->user_locations()->sync($newLocations);
+        $data = [
+            'user' => $updatedUser,
+            'user_detail' => $updatedUserDetail,
+        ];
 
-        // Update Assigning WorkTypes to User
-        $newWorkTypes = $request->input('work_type_id') ?? $user->work_types()->pluck('work_types.id')->toArray();
-        $user->work_types()->sync($newWorkTypes);
+        return $this->returnData("data", $data, "User Updated");
 
-        return $this->returnData("data", $finalData, "User Updated");
+        // // Update Assigning Roles to User
+        // $newRoles = $request->input('roles') ?? $user->getRoleNames()->toArray();
+        // $user->syncRoles($newRoles);
+
+        // // Update Assigning Locations to User
+        // $newLocations = $request->input('location_id') ?? $user->user_locations()->pluck('locations.id')->toArray();
+        // $user->user_locations()->sync($newLocations);
+
+        // // Update Assigning WorkTypes to User
+        // $newWorkTypes = $request->input('work_type_id') ?? $user->work_types()->pluck('work_types.id')->toArray();
+        // $user->work_types()->sync($newWorkTypes);
+
+        // Assign roles, locations, and work types
+        // $this->assignRoles($user, $request->input('roles', []));
+        // $this->assignLocations($user, $request->input('location_id', []));
+        // $this->assignWorkTypes($user, $request->input('work_type_id', []));
+        // return $this->returnData("data", $finalData, "User Updated");
     }
 
     public function destroy(User $user)
@@ -347,22 +209,6 @@ class UserController extends Controller
         return $this->returnData("user", $user, "user deleted");
     }
 
-    public function logout()
-    {
-        $user = auth()->user();
-        Auth::logout();
-        return $this->returnData("user", $user, "You are logged out");
-    }
-    public function profile()
-    {
-        $authUser = Auth::user();
-        $user = User::where('id', $authUser->id)->first();
-        if (!$user) {
-            return $this->returnError('No User Found');
-        }
-
-        return $this->returnData("User", new LoginResource($user), "User Data");
-    }
     public function getAllUsersNames()
     {
         $usersByName = User::pluck('name');
