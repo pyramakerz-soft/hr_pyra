aea03f603f948448f4ab3138e369036ef3b83496
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2024-09-04 08:40:24 +0300

    edit export

--- app/Http/Controllers/Api/ClockController.php
+++ app/Http/Controllers/Api/ClockController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exports\ClocksExport;
 use App\Exports\UserClocksExportById;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreClockInOutRequest;
@@ -146,8 +147,8 @@ class ClockController extends Controller
             $clocksCollection = $clocks instanceof \Illuminate\Pagination\LengthAwarePaginator
             ? $clocks->getCollection()
             : $clocks;
+            return (new ClocksExport)->download('all_user_clocks.xlsx');
 
-            return Excel::download(new UserClocksExportById($clocksCollection), 'all_user_clocks.xlsx');
         }
 
         $data = $this->prepareClockData($clocks);
@@ -164,54 +165,56 @@ class ClockController extends Controller
         if (!$authUser->hasRole('Hr')) {
             return $this->returnError('You are not authorized to view user clocks', 403);
         }
-
+    
         $query = ClockInOut::where('user_id', $user->id);
-
+    
         if ($request->has('date')) {
             $date = Carbon::parse($request->get('date'))->toDateString();
             $query->whereDate('clock_in', $date);
             $clocks = $query->orderBy('clock_in', 'desc')->get();
-
+    
         } else if ($request->has('month')) {
             $month = Carbon::parse($request->get('month'));
-
+    
             $startOfMonth = $month->copy()->subMonth()->startOfMonth()->addDays(25);
-
             $endOfMonth = $month->copy()->startOfMonth()->addDays(25);
-
+    
             $query->whereBetween('clock_in', [$startOfMonth, $endOfMonth]);
-
+    
             $clocks = $query->orderBy('clock_in', 'desc')->paginate(7);
         } else {
-
             $now = Carbon::now();
-
+    
             $currentStart = $now->copy()->subMonth()->startOfMonth()->addDays(25);
             $currentEnd = $now->copy()->startOfMonth()->addDays(25);
-
-            // Filter based on the current custom month range
+    
             $query->whereBetween('clock_in', [$currentStart, $currentEnd]);
-
-            // Paginate and sort by clock_in in descending order
+    
             $clocks = $query->orderBy('clock_in', 'desc')->paginate(7);
         }
-
+    
         if ($clocks->isEmpty()) {
             return $this->returnError('No Clocks Found For This User');
         }
-
+    
+        // Handle Export Request
         if ($request->has('export')) {
-            $clocksCollection = ($clocks->getCollection());
-            return Excel::download(new UserClocksExportById($clocksCollection), 'user_clocks.xlsx');
+            $clocksCollection = $clocks instanceof \Illuminate\Pagination\LengthAwarePaginator
+                ? $clocks->getCollection()
+                : $clocks;
+    
+            return Excel::download(new UserClocksExportById($clocksCollection, $user->name), 'user_clocks_' . $user->name . '.xlsx');
         }
+    
         $data = $this->prepareClockData($clocks);
-
+    
         if (!isset($data['clocks'])) {
             return $this->returnError('No Clocks Found For This User');
         }
+    
         return $this->returnData("data", $data, "Clocks Data for {$user->name}");
     }
-
+    
     public function clockIn(StoreClockInOutRequest $request)
     {
         $authUser = Auth::user();
