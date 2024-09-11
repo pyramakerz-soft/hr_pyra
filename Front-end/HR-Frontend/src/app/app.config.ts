e9eedba36e80c6b3a96bada524d5b1d8e8b09810
pyramakerzgit@gmail.com <pyramakerzgit@gmail.com>
2024-09-11 10:48:43 +0000

    edit

--- Front-end/HR-Frontend/src/app/app.config.ts
+++ Front-end/HR-Frontend/src/app/app.config.ts
@@ -4,13 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'; // Import HashLocationStrategy and LocationStrategy
+
 // import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  // providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes) , provideRouter(routes),provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync()]
-
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), provideRouter(routes),provideHttpClient(),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
     provideClientHydration(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy } // Add HashLocationStrategy to providers
   ]
 };
--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -43,10 +43,6 @@ class ProfileResource extends JsonResource
         }
 
         $work_home = false;
-        $locationTypes = $authUser->work_types->pluck('name');
-        if (count($locationTypes) > 1) {
-            $work_home = true;
-        }
         $user_locations = $authUser->user_locations()->get();
         $locations_name = $user_locations->map(function ($user_location) {
             return [
@@ -58,6 +54,7 @@ class ProfileResource extends JsonResource
             ];
         });
 
+
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -68,11 +65,12 @@ class ProfileResource extends JsonResource
             'is_clocked_out' => $is_clocked_out,
             'clockIn' => $clockIn,
             'total_hours' => $total_hours,
-            'work_home' => $work_home,
             'user_start_time' => $this->user_detail->start_time,
             'user_end_time' => $this->user_detail->end_time,
 
             'assigned_locations_user' => $locations_name,
+            'assignedLocationsUser' => $locations_name,
+            'work_home' => isset($locationTypes[0]) ? $locationTypes[0] == 'home' : false,
         ];
 
     }
