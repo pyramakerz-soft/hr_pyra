a7c31335e53e5dde6f1e6d5dc50b0fec0e9ee93d
pyramakerzgit@gmail.com <pyramakerzgit@gmail.com>
2024-09-18 06:37:34 +0000

    Routes

--- Front-end/HR-Frontend/src/app/app.routes.ts
+++ Front-end/HR-Frontend/src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
 import { EmployeeDashboardComponent } from './Pages/employee-dashboard/employee-dashboard.component';
 import { EmployeeComponent } from './Pages/Employee/employee/employee.component';
@@ -21,36 +22,6 @@ import { HrDepartmentComponent } from './Pages/HR/hr-department/hr-department.co
 import { HrDepartmentAddComponent } from './Pages/HR/hr-department-add/hr-department-add.component';
 import { UserDataService } from './Services/Resolvers/user-data.service';
 
-// export const routes: Routes = [
-//     {path: "employee", component:EmployeeComponent, title:"Dashboard", children:[
-//         {path: "", redirectTo: "Dashboard", pathMatch: "full" },
-//         {path: "Dashboard", component:EmployeeDashboardComponent, title:"Dashboard", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfEmployeeGuard] },
-//     ], canActivate:[doNotNavigateWithoutLoginGuard, navigateIfEmployeeGuard]},
-
-//     {path: "HR", component:HRComponent, title:"HR", children:[
-//         {path: "", redirectTo: "HRDashboard", pathMatch: "full"},
-//         {path: "HRDashboard", component:HrDashboardComponent, title:"HRDashBoard", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HREmployee", component:HREmployeeComponent, title:"HREmployee", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HRRole", component:HrRoleComponent, title:"HRRole", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HRRoleAdd", component:HrRoleAddComponent, title:"HRRoleAdd", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HRRoleEdit/:id", component:HrRoleAddComponent, title:"HRRoleEdit", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HRBounders", component:HrBoundersComponent, title:"HRBounders", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HRAttendance", component:HrAttendanceComponent, title:"HRAttendance", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HRAttendanceEmployeeDetails/:Id", component:HrEmployeeAttendanceDetailsComponent, title:"HREmployeeAttendanceDetails", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard], resolve:{user:UserDataService}},
-//         {path: "HREmployeeDetails/:EmpId", component:HrEmployeeDetailsComponent, title:"HREmployeeDetails", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HREmployeeDetailsAdd", component:HrEmployeeAddEditDetailsComponent, title:"HREmployeeDetailsAdd", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard] },
-//         {path: "HREmployeeDetailsEdit/:Id", component:HrEmployeeAddEditDetailsComponent, title:"HREmployeeDetailsEdit", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HRAttendanceEmployeeEdit/:Id", component:AttendenceEditComponent, title:"HRAttendanceEmployeeEdit", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HRDepartment", component:HrDepartmentComponent, title:"HRDepartment", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HRDepartmentAdd", component:HrDepartmentAddComponent, title:"HRDepartmentAdd", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-//         {path: "HRDepartmentEdit/:id", component:HrDepartmentAddComponent, title:"HRDepartmentEdit", canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-
-//     ], canActivate:[doNotNavigateWithoutLoginGuard, navigateIfHrGuard]},
-
-//     { path: "Login", component:LoginComponent, title:"Login", canActivate:[doNotNavigateToLoginIfTokenExistsGuard] },
-//     { path: "", component:LoginComponent, title:"Login", canActivate:[doNotNavigateToLoginIfTokenExistsGuard] },
-//     { path: '**', redirectTo: '/' },
-// ];
 export const routes: Routes = [
     { 
         path: "employee", 
@@ -95,5 +66,11 @@ export const routes: Routes = [
 
     { path: "Login", component:LoginComponent, title:"Login", canActivate:[doNotNavigateToLoginIfTokenExistsGuard] },
     { path: "", redirectTo: "Login", pathMatch: "full" },
-    { path: '**', redirectTo: '/' },
+    { path: '**', redirectTo: '/' }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
