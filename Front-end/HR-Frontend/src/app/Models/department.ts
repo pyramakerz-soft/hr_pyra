de520d0e5c8de767c95e771f2870340d619f9098
mennausama <mennausama1682@gmail.com>
2024-09-23 12:53:15 +0300

    is notify in depoartment

--- Front-end/HR-Frontend/src/app/Models/department.ts
+++ Front-end/HR-Frontend/src/app/Models/department.ts
@@ -3,6 +3,7 @@ export class Department {
         public id: number,
         public name: string,
         public manager_id: number,
-        public manager_name :string
+        public manager_name :string,
+        public is_location_time : boolean
     ) {}
 }
