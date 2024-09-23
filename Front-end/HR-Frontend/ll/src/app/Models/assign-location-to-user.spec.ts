9238e1a2f94550e18abf87c30a1f570b2c4baf6f
pyramakerzgit@gmail.com <pyramakerzgit@gmail.com>
2024-09-23 08:52:21 +0000

    Deleted un-needed files

--- Front-end/HR-Frontend/ll/src/app/Models/assign-location-to-user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AssignLocationToUser } from './assign-location-to-user';
-
-describe('AssignLocationToUser', () => {
-  it('should create an instance', () => {
-    expect(new AssignLocationToUser()).toBeTruthy();
-  });
-});
--- Front-end/HR-Frontend/ll/src/app/Models/permission-add-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PermissionAddModel } from './permission-add-model';
-
-describe('PermissionAddModel', () => {
-  it('should create an instance', () => {
-    expect(new PermissionAddModel()).toBeTruthy();
-  });
-});
