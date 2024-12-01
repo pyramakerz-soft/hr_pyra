fc1cf8a1d705d49a734da1cb56cae27200183819
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2024-12-01 10:05:35 +0200

    add work type in profile api

--- app/Http/Resources/Api/ProfileResource.php
+++ app/Http/Resources/Api/ProfileResource.php
@@ -102,6 +102,8 @@ class ProfileResource extends JsonResource
             'is_notify_by_location' => $this->isNotifyByLocation(),
             'assigned_locations_user' => $this->getAssignedLocations($authUser),
             'work_home' => $this->isWorkingFromHome($authUser),
+            'work_types' => $this->work_types->pluck('name')->toArray(), 
+
             // 'is_float' => $this->user_detail->is_float == 1 ? true : false,
 
         ];
