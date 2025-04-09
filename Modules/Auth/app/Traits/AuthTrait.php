1b94ff7576d7cec8e9d81c0fa3d0d22f63490a5d
ZeyadShawki <zizoelfager14@gmail.com>
2025-04-09 14:00:51 +0200

    LOG SERIAL

--- Modules/Auth/app/Traits/AuthTrait.php
+++ Modules/Auth/app/Traits/AuthTrait.php
@@ -5,6 +5,7 @@ namespace Modules\Auth\Traits;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rule;
 use Modules\Users\Models\User;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -21,67 +22,72 @@ trait AuthTrait
 
     //     return null; // Return null if user not found or password is incorrect
     // }
-    
+
     protected function validateUser(array $credentials)
-{
-    $user = User::whereRaw('LOWER(email) = ?', [strtolower($credentials['email'])])->first();
+    {
+        $user = User::whereRaw('LOWER(email) = ?', [strtolower($credentials['email'])])->first();
 
-    if ($user && Hash::check($credentials['password'], $user->password)) {
-        return $user;
+        if ($user && Hash::check($credentials['password'], $user->password)) {
+            return $user;
+        }
+
+        return null;
     }
-
-    return null;
-}
 
 
 
     protected function validateSerialNumber(Request $request, User $user)
-{
-    if ($request->serial_number ) {
-        
-        // Check if the serial number doesn't contain "#" (indicating an outdated version)
-        if (strpos($request->serial_number, '#') === false) {
-            throw new \Exception('Please update the app to the latest version to continue.', 406);
-        }
+    {
+        if ($request->serial_number) {
 
-        if (is_null($user->serial_number)) {
-            $request->validate([
-                'serial_number' => [Rule::unique('users', 'serial_number')->ignore($user->id)],
-            ]);
-            $user->update(['serial_number' => $request->serial_number]);
-        } 
-        // If user already has a serial number but it doesn't contain "#", update it
-        elseif (strpos($user->serial_number, '#') === false) {
-            $user->update(['serial_number' => $request->serial_number]);
-        } 
-        // If the user's serial number is different from the request serial number, throw an error
-        elseif ($user->serial_number !== $request->serial_number) {
-            throw new \Exception('Serial number does not match', 406);
-        }
+            // Check if the serial number doesn't contain "#" (indicating an outdated version)
+            if (strpos($request->serial_number, '#') === false) {
+                throw new \Exception('Please update the app to the latest version to continue.', 406);
+            }
 
-        // Handle mobile verification
-        
+            if (is_null($user->serial_number)) {
+                $request->validate([
+                    'serial_number' => [Rule::unique('users', 'serial_number')->ignore($user->id)],
+                ]);
+                $user->update(['serial_number' => $request->serial_number]);
+            }
+            // If user already has a serial number but it doesn't contain "#", update it
+            elseif (strpos($user->serial_number, '#') === false) {
+                $user->update(['serial_number' => $request->serial_number]);
+            }
+            // If the user's serial number is different from the request serial number, throw an error
+            elseif ($user->serial_number !== $request->serial_number) {
+                Log::info('SERIAL COMPRISON');
+
+                Log::info('user SERIAL '.$user->serial_number );
+                Log::info('REQUEST SERIAL '.$request->serial_number);
+
+                throw new \Exception('Serial number does not match', 406);
+            }
+
+            // Handle mobile verification
+
+        }
     }
-}
 
     protected function generateToken(Request $request, User $user)
-{
-    // Convert email to lowercase before attempting authentication
-    $credentials = [
-        'email' => strtolower($request->email),
-        'password' => $request->password
-    ];
+    {
+        // Convert email to lowercase before attempting authentication
+        $credentials = [
+            'email' => strtolower($request->email),
+            'password' => $request->password
+        ];
 
-    // Attempt authentication with lowercase email
-    $token = JWTAuth::attempt($credentials);
+        // Attempt authentication with lowercase email
+        $token = JWTAuth::attempt($credentials);
 
-    if (!$token) {
-        throw new \Exception('You are unauthenticated', Response::HTTP_UNAUTHORIZED);
+        if (!$token) {
+            throw new \Exception('You are unauthenticated', Response::HTTP_UNAUTHORIZED);
+        }
+
+        // Return the generated token
+        return $token;
     }
-
-    // Return the generated token
-    return $token;
-}
 
     protected function respondWithToken($token)
     {
