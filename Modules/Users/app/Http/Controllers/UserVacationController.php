a4666aeb7bd8d2948d1c0a10cf6548f34f1330ea
Mostafa <mostafaa7med1000@gmail.com>
2026-01-19 10:42:25 +0200

    add declined to the balance check ignore

--- Modules/Users/app/Http/Controllers/UserVacationController.php
+++ Modules/Users/app/Http/Controllers/UserVacationController.php
@@ -336,7 +336,7 @@ class UserVacationController extends Controller
                     $allocatedDays = $vacation->vacationType->default_days ?? 0;
                     $remainingDays = max(0, $allocatedDays - $usedDays);
 
-                    if ($remainingDays < ($vacation->days_count ?? 0) && $status !== StatusEnum::Refused->value && $status !== StatusEnum::Rejected->value) {
+                    if ($remainingDays < ($vacation->days_count ?? 0) && $status !== StatusEnum::Refused->value && $status !== StatusEnum::Rejected->value && $status !== StatusEnum::Declined->value) {
                         return $this->returnError('Insufficient ' . $vacation->vacationType->name . ' balance. Remaining days: ' . $remainingDays, 422);
                     }
 
@@ -344,14 +344,14 @@ class UserVacationController extends Controller
                     $annualType = VacationType::where('name', self::ANNUAL_LEAVE_NAME)->first();
                     if ($annualType) {
                         $annualBalance = $this->getOrCreateBalance($vacation->user, $annualType, Carbon::parse($vacation->from_date));
-                        if ($annualBalance->remaining_days < ($vacation->days_count ?? 0) && $status !== StatusEnum::Refused->value && $status !== StatusEnum::Rejected->value) {
+                        if ($annualBalance->remaining_days < ($vacation->days_count ?? 0) && $status !== StatusEnum::Refused->value && $status !== StatusEnum::Rejected->value && $status !== StatusEnum::Declined->value) {
                             return $this->returnError('Insufficient ' . self::ANNUAL_LEAVE_NAME . ' balance. Remaining days: ' . $annualBalance->remaining_days, 422);
                         }
                     }
                 } elseif ($vacation->vacationType->name !== self::UNPAID_LEAVE_NAME && $vacation->vacationType->name !== self::SICK_LEAVE_NAME && $vacation->vacationType->name !== self::EXCEPTIONAL_LEAVE_NAME) {
                     // Standard check for other types (excluding Unpaid)
                     $balance = $this->getOrCreateBalance($vacation->user, $vacation->vacationType, Carbon::parse($vacation->from_date));
-                    if ($balance->remaining_days < ($vacation->days_count ?? 0) && $status !== StatusEnum::Refused->value && $status !== StatusEnum::Rejected->value) {
+                    if ($balance->remaining_days < ($vacation->days_count ?? 0) && $status !== StatusEnum::Refused->value && $status !== StatusEnum::Rejected->value && $status !== StatusEnum::Declined->value) {
                         return $this->returnError('Insufficient ' . $vacation->vacationType->name . ' balance. Remaining days: ' . $balance->remaining_days, 422);
                     }
                 }
